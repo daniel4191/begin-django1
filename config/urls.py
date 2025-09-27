3b0683baeff3766ff94ff66bc8906f02005eede4
daniel4191 <daniell8282@naver.com>
2025-09-27 23:15:12 +0900

    댓글 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import index
+from blog.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index)
+    path("", index),
+    path("posts/", post_list)
 ]
