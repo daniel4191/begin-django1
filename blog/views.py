2ce3d0aba21dc69bffd58114dba180e0fac6abd1
daniel4191 <daniell8282@naver.com>
2025-09-30 23:00:08 +0900

    글 생성 기능

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Post
 
@@ -9,3 +9,24 @@ def post_list(request):
         "posts":posts
     }
     return render(request, "post_list.html", context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    print(post)
+    context = {
+        "post":post
+    }
+    return render(request, "post_detail.html", context)
+
+def post_add(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post.objects.create(
+            title = title,
+            content = content
+        )
+        return redirect(f"/posts/{post.id}")
+        
+    return render(request, "post_add.html")
+        
